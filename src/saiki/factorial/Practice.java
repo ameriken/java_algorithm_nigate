f3c414f1e9a60a02e20720afca6b3d5444e4505f
ameriken <hitohahito@hotmail.co.jp>
2020-05-01 00:42:11 +0900

    練習用こーどを記載

--- src/saiki/factorial/Practice.java
+++ src/saiki/factorial/Practice.java
@@ -1,4 +1,15 @@
 package saiki.factorial;
 
 public class Practice {
+    private static long factorial(int number) {
+        // コードを書く
+        return (Long) 1L;
+    }
+
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i ++) {
+            Long value = factorial(i);
+            System.out.println(i + "の階乗は " + value);
+        }
+    }
 }
