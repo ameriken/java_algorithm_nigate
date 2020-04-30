d3f10cad1230756fa7709055dd81561080a500ba
ameriken <hitohahito@hotmail.co.jp>
2020-05-01 01:17:35 +0900

    練習コード

--- src/saiki/fibo/Main.java
+++ src/saiki/fibo/Main.java
@@ -7,11 +7,13 @@ public class Main {
         if(n < 2) {
             return n;
         } else {
-            return fibo(n - 1) + fibo(n - 2);
+            int fibo1 = fibo(n-1);
+            int fibo2 = fibo(n-2);
+            return fibo1 + fibo2;
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(fibo(10));
+        System.out.println(fibo(3));
     }
 }
--- src/saiki/fibo/Practice.java
+++ src/saiki/fibo/Practice.java
@@ -1,4 +1,12 @@
 package saiki.fibo;
 
 public class Practice {
+    private static int fibo(int n) {
+        // ここにコードを書く
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibo(3));
+    }
 }
