4587d8311c7b03e34ea700457ba0b0745d6959e1
ameriken <hitohahito@hotmail.co.jp>
2020-05-01 00:08:50 +0900

    binary_searchの練習

---
+++ src/search/binary_search/Practice.java
@@ -0,0 +1,13 @@
+package search.binary_search;
+
+public class Practice {
+
+
+    public static void main(String[] args) {
+        int a[] = {1,2,4,5,10};
+        int searchValue = 7;
+        int findId = -1;
+
+        System.out.println("見つかった番号 = " + findId);
+    }
+}
