3e87dab77ba8e8d858d954934cbe89d88818421f
ameriken <hitohahito@hotmail.co.jp>
2020-05-01 11:49:12 +0900

    最大公約数-練習

---
+++ src/saiki/gcd/Practice.java
@@ -0,0 +1,22 @@
+package saiki.gcd;
+
+public class Practice {
+    public static Long gcd(Long a, Long b) {
+        // ここにこーどを書く
+        return (Long)1L;
+    }
+
+
+    public static void main(String[] args) {
+        //コード
+        Long a = 3L;
+        Long b = 15L;
+
+        Long sum = gcd(a, b);
+
+        // 最大公倍数
+        System.out.println(sum);
+
+        // (与えられた整数 * 与えられた) / 整数最大公倍数 = 最小公倍数
+    }
+}
