6dab3c98e4e5823b129538848567a7d9563904aa
ameriken <hitohahito@hotmail.co.jp>
2020-05-01 11:03:37 +0900

    順列を作成

--- src/saiki/permutation/Main.java
+++ src/saiki/permutation/Main.java
@@ -1,4 +1,32 @@
 package saiki.permutation;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+
+    public static List<String> permutation(String list) {
+        List<String> ans = new ArrayList<>();
+        if (list.length() == 1) {
+            return Collections.singletonList(list);
+        }
+        for (int i = 0; i < list.length(); i++) {
+            char pick = list.charAt(i);
+            String rest = list.substring(0, i) + list.substring(i + 1);
+            for (String rest_permutation : permutation(rest)) {
+                ans.add(pick + rest_permutation);
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        //コード
+        String number = "";
+        for(int i = 1; i <= 5; i++) {
+            number += i;
+        }
+
+        System.out.println(permutation(number));
+    }
 }
