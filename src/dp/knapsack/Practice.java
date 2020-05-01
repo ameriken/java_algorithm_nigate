551e9ce3388b14d8c7b49c9c5d3079384d6746f1
ameriken <hitohahito@hotmail.co.jp>
2020-05-01 11:55:39 +0900

    ナップザック練習問題を追加

--- src/dp/knapsack/Practice.java
+++ src/dp/knapsack/Practice.java
@@ -2,4 +2,18 @@ package dp.knapsack;
 
 public class Practice {
     // ここで書いていき覚えていく
+    private final int N;
+    private final int[] size;
+    private final int[] value;
+
+
+    public Practice(int[] size, int[] value) {
+        this.N = size.length;
+        this.size = (int[])size.clone();
+        this.value = (int[])value.clone();
+    }
+
+    public void solve(int m) {
+
+    }
 }
