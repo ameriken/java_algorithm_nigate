c0bc9e6883431b2d0233d1700965b73e3cbc0d09
ameriken <hitohahito@hotmail.co.jp>
2020-05-02 17:46:13 +0900

    変数の追加

--- src/graph/weighted/WeightedGraph.java
+++ src/graph/weighted/WeightedGraph.java
@@ -5,12 +5,12 @@ public class WeightedGraph {
     private static String[] stations = {"横浜", "武蔵小杉","品川","渋谷","新橋","溜池山王"};
 
     private static int[][] adjacencyMatrix = {
-            new int[]{ 0,12,28, 0,0,0},
-            new int[]{12, 0,10,13,0,0},
-            new int[]{28,10, 0,11,7,0},
-            new int[]{ 0,13,11, 0,0,9},
-            new int[]{ 0, 0, 7, 0,0,4},
-            new int[]{ 0, 0, 0, 9,4,0}
+            new int[]{ 0,12,28, 0, 0, 0},
+            new int[]{12, 0,10,13, 0, 0},
+            new int[]{28,10, 0,11, 7, 0},
+            new int[]{ 0,13,11, 0, 0, 9},
+            new int[]{ 0, 0, 7, 0, 0, 4},
+            new int[]{ 0, 0, 0, 9, 4, 0}
     };
 
     public static void main(String[] args) {
