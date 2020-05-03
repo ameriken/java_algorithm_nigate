508635b9dac750c23a2ef02854e2b40f376c821e
ameriken <hitohahito@hotmail.co.jp>
2020-05-04 01:17:04 +0900

    コメントを追加

--- src/graph/bellman_ford/BellmanFord.java
+++ src/graph/bellman_ford/BellmanFord.java
@@ -24,16 +24,17 @@ public class BellmanFord {
             }
         }
 
-        for (int i = 0; i < E; i++)
-        {
-            int x = graph[i][0];
-            int y = graph[i][1];
-            int weight = graph[i][2];
-            if (dis[x] != Integer.MAX_VALUE && dis[x] + weight < dis[y]) {
-                System.out.println("Graph contains negative"
-                        +" weight cycle");
-            }
-        }
+        // ひとまず暗記不要
+        //for (int i = 0; i < E; i++)
+        //{
+        //    int x = graph[i][0];
+        //    int y = graph[i][1];
+        //    int weight = graph[i][2];
+        //    if (dis[x] != Integer.MAX_VALUE && dis[x] + weight < dis[y]) {
+        //        System.out.println("Graph contains negative"
+        //                +" weight cycle");
+        //    }
+        //}
 
         System.out.println("原点からそれぞれの頂点の距離は.....");
         for (int i = 0; i < V; i++)
