560dbc13916ecc46cb5f239deb6849426a9f9ec7
ameriken <hitohahito@hotmail.co.jp>
2020-05-05 00:55:32 +0900

    imos法

---
+++ src/imos/Practice.java
@@ -0,0 +1,16 @@
+package imos;
+
+public class Practice {
+
+
+    public static void main(String[] args) {
+        // アンケートの数
+        int n = 4;
+        // 0 - 1,000,000通りの濃さがある
+        // a = 濃さの始点, b = 濃さの終点
+        int a_b[][] = { {0, 2}, {2, 3}, {2, 4}, {5, 6}};
+        int[] S = new int[1000001];
+
+    }
+
+}
