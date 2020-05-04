adea973870f2c8cc71fc72da3de93296d6ea0a1a
ameriken <hitohahito@hotmail.co.jp>
2020-05-04 11:03:01 +0900

    imos法ベース

---
+++ src/imos/Main.java
@@ -0,0 +1,30 @@
+package imos;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // アンケートの数
+        int n = 4;
+        // 0 - 1,000,000通りの濃さがある
+        // a = 濃さの始点, b = 濃さの終点
+        int a_b[][] = { {0, 2}, {2, 3}, {2, 4}, {5, 6}};
+        int[] S = new int[1000001];
+
+        // 始点に+1終点-1をいれる
+        for (int i = 0; i < a_b.length; i++) {
+            S[a_b[i][0]] += 1;
+            if(a_b[i][1] != 1000000) {
+                S[a_b[i][1] + 1] -= 1;
+            }
+        }
+
+        int max = S[0];
+        for(int i=1; i<=1000000; i++){
+            S[i] += S[i - 1];
+            max = Math.max(max, S[i]);
+        }
+        System.out.println(max);
+    }
+
+}
