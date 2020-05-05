1476ecdf6f1b2764c50a5e424e2c640325aa8826
ameriken <hitohahito@hotmail.co.jp>
2020-05-05 14:54:25 +0900

    練習問題追加

---
+++ src/union_find/Practice.java
@@ -0,0 +1,32 @@
+
+package union_find;
+
+public class Practice {
+
+    int[] uni;
+
+    //-1で初期化
+    Practice(int size){
+        uni = new int[size];
+        for(int i=0;i<size;i++) uni[i] = -1;
+    }
+
+    // 頂点aの所属するグループを調べる
+    int root(int a) {
+        //ここにコードをかく
+        return 1;
+    }
+
+    boolean connect(int a, int b) {
+        //ここにコードをかく
+        return false;
+    }
+
+    boolean isConnect(int a, int b) {
+        return root(a) == root(b);
+    }
+
+    int size(int a) {
+        return -uni[root(a)];
+    }
+}
