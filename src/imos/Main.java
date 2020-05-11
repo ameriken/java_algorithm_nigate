c2b2684eca711661a99e5b25ec2fa0736fbce392
ameriken <hitohahito@hotmail.co.jp>
2020-05-12 00:28:37 +0900

    説明文の追加

--- src/imos/Main.java
+++ src/imos/Main.java
@@ -8,9 +8,9 @@ public class Main {
     public static void main(String[] args) {
         // アンケートの数
         int n = 4;
-        // 0 - 1,000,000通りの濃さがある
         // a = 濃さの始点, b = 濃さの終点
         int a_b[][] = { {0, 2}, {2, 3}, {2, 4}, {5, 6}};
+        // 0 - 1,000,000通りの濃さがある
         int[] S = new int[1000001];
 
         // 始点に+1終点-1をいれる
