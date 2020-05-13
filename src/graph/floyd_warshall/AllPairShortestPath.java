656ce276edff120e93f2437f10509564d2234e66
ameriken <hitohahito@hotmail.co.jp>
2020-05-13 10:34:24 +0900

    intの宣言を削除

--- src/graph/floyd_warshall/AllPairShortestPath.java
+++ src/graph/floyd_warshall/AllPairShortestPath.java
@@ -4,22 +4,17 @@ public class AllPairShortestPath {
 
     void execute(int graph[][], int V, int INF) {
         int dist[][] = new int[V][V];
-        int i, j, k;
 
         // ここでdistを初期化する
-        for (i = 0; i < V; i++) {
-            for (j = 0; j < V; j++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
                 dist[i][j] = graph[i][j];
             }
         }
 
-        for (k = 0; k < V; k++) {
-            for (i = 0; i < V; i++) {
-                for (j = 0; j < V; j++) {
-                    //System.out.printf("dist[%d][%d]=%d%n", i,k, dist[i][k]);
-                    //System.out.printf("dist[%d][%d]=%d%n", k,j, dist[k][j]);
-                    //System.out.print(">>>>>>>>>>>>>>>>>>>>");
-                    //System.out.printf("dist[%d][%d]=%d%n", i,j, dist[i][j]);
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
                     if (dist[i][k] + dist[k][j] < dist[i][j]) {
                         dist[i][j] = dist[i][k] + dist[k][j];
                     }
