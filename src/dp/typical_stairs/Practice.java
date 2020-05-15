337f9ed9c7472ea9de23edf74dfcf7cb33881779
ameriken <hitohahito@hotmail.co.jp>
2020-05-15 23:12:18 +0900

    練習こーどを追加

--- src/dp/typical_stairs/Practice.java
+++ src/dp/typical_stairs/Practice.java
@@ -1,18 +1,14 @@
 package dp.typical_stairs;
 
 import java.util.Arrays;
-import java.util.Scanner;
-
 
 
 // 問題URL
 // https://atcoder.jp/contests/abc129/tasks/abc129_c
 // https://drken1215.hatenablog.com/entry/2019/06/10/140000
 
-import java.util.Arrays;
-import java.util.Scanner;
 
-public class Main {
+public class Practice {
     private static int MOD = 1000000007;
     public static void main(String[] args) {
         //コード
@@ -25,18 +21,8 @@ public class Main {
             isSafe[A[i]] = false;
         }
 
-        Integer[] dp = new Integer[N+1];
-        Arrays.fill(dp, 0);
-        dp[0] = 1;
-        if(A.length == 0 || isSafe[1]) dp[1] = 1;
+        // ここにコードを書いてください。
 
-
-        for(int i = 2; i <= N; i++) {
-            if (isSafe[i-1]) dp[i] += dp[i-1];
-            if (isSafe[i-2]) dp[i] += dp[i-2];
-            dp[i] %= MOD; // 1000000007 で割る
-        }
-        System.out.println(dp[N]);
     }
 }
 
