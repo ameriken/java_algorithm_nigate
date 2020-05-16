298289ec6524d4dd037b0fab2dfddd1924699ab8
ameriken <hitohahito@hotmail.co.jp>
2020-05-17 03:28:43 +0900

    少し短縮

--- src/search/dfs_graph/Main.java
+++ src/search/dfs_graph/Main.java
@@ -13,8 +13,7 @@ class Main {
         Dfs dfs = new Dfs(n);
 
         for(int i = 0;i < n;i++){
-            int u = input.nextInt();
-            int k = input.nextInt();
+            int u = input.nextInt(),k = input.nextInt();
             dfs.graph[u - 1] = new int[k];
             for(int j = 0;j < k;j++){
                 dfs.graph[u - 1][j] = input.nextInt() - 1;
