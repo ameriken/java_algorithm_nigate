7d36439999d0066b5150728cd950a900087692af
ameriken <hitohahito@hotmail.co.jp>
2020-05-17 13:36:22 +0900

    bfsのグラフを作成

--- src/search/bfs_graph/Main.java
+++ src/search/bfs_graph/Main.java
@@ -1,25 +1,60 @@
 package search.bfs_graph;
 
 
-import static search.bfs_graph.Bfs.longestPath;
-//import static search.bfs.Practice.longestPath;
+import java.util.*;
+
+/*
+入力：
+4 5
+0 3
+0 1
+0 2
+1 3
+2 1
+2 3
+出力：
+yes
+*/
 
 public class Main {
 
+    static boolean[] seen;
     public static void main(String[] args) {
 
-        //コード
-        String[] maze = {
-                "...",
-                "...",
-                "..."
-        };
+        Scanner scanner = new Scanner(System.in);
+        Integer V = Integer.parseInt(scanner.next());
+        Integer E = Integer.parseInt(scanner.next());
+        Integer s = Integer.parseInt(scanner.next());
+        Integer t = Integer.parseInt(scanner.next());
 
-        int startRow= 0;
-        int startCol= 1;
-        int[] moveRow = {1, 0, -1, 0};
-        int[] moveCol = {0, 1, 0, -1};
+        seen = new boolean[V];
+        Arrays.fill(seen, false);
 
-        System.out.println(longestPath(maze, startRow, startCol, moveRow, moveCol));
+        LinkedList<Integer>[] graph = new LinkedList[V];
+        for (int i=0; i<V; ++i) graph[i] = new LinkedList();
+
+        for (int i = 0; i < E; i++) {
+            Integer a = Integer.parseInt(scanner.next());
+            graph[a].add(Integer.parseInt(scanner.next()));
+        }
+
+        Queue<Integer> que = new LinkedList<>();
+        que.add(s);
+        seen[s] = true;
+
+        while(que.size() != 0) {
+            Integer state = que.poll();
+            for (Integer next : graph[state]) {
+                if (!seen[next]) {
+                    seen[next] = true;
+                    que.add(next);
+                }
+            }
+        }
+        if (seen[t]) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
     }
 }
--- src/search/dfs_graph/Main.java
+++ src/search/dfs_graph/Main.java
@@ -28,14 +28,14 @@ class Main {
 
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        Integer V = Integer.parseInt(scanner.next());
-        Integer E = Integer.parseInt(scanner.next());
-        Integer s = Integer.parseInt(scanner.next());
-        Integer t = Integer.parseInt(scanner.next());
+        Integer V = Integer.parseInt(scanner.next()), E = Integer.parseInt(scanner.next());
+        Integer s = Integer.parseInt(scanner.next()), t = Integer.parseInt(scanner.next());
 
         seen = new boolean[V];
         LinkedList<Integer> graph[] = new LinkedList[V];
-        for (int i=0; i<V; ++i) graph[i] = new LinkedList();
+        for (int i=0; i<V; ++i) {
+            graph[i] = new LinkedList();
+        }
 
         for (int i = 0; i < E; i++) {
             Integer a = Integer.parseInt(scanner.next());
