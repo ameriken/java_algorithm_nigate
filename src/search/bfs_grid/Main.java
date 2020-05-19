bcc6de8cf19756aa3a2e80469607e00e827ece4f
ameriken <hitohahito@hotmail.co.jp>
2020-05-19 17:13:32 +0900

    説明追加

--- src/search/bfs_grid/Main.java
+++ src/search/bfs_grid/Main.java
@@ -14,10 +14,13 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // マスの縦横の大きさ R:縦 C:幅
         Integer R = Integer.parseInt(scanner.next());
         Integer C = Integer.parseInt(scanner.next());
+        // スタート地点の座標 sy:縦 sx:幅
         Integer sy = Integer.parseInt(scanner.next()) - 1;
         Integer sx = Integer.parseInt(scanner.next()) - 1;
+        // ゴールの座標 gy:縦 gx:幅
         Integer gy = Integer.parseInt(scanner.next()) - 1;
         Integer gx = Integer.parseInt(scanner.next()) - 1;
 
